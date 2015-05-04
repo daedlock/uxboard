8a5eec3738390b47d34cd475d9e8b1b0afd58769
Hossam Saraya <hossam.saraya@gmail.com>
2015-05-04 16:57:26 +0300

    UI

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
 var Article = require('../models/article');
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  Article.find().lean().exec(function (err, docs) {
 
-  Article.find().exec(function (err, articles) {
-    res.render('index', { articles: articles });
+    res.render('index', { articles: docs });
   })
 });
 
