cb7735d87ea69dcd4f01dc39c9af26cd3e8efe08
Hossam Saraya <hossam.saraya@gmail.com>
2015-05-16 20:52:56 +0300

    Added tests for rgbtoday and smashingmagazine

--- test/scrapersSpec.js
+++ test/scrapersSpec.js
@@ -1,23 +1,41 @@
-var chai = require('chai')
-  , chaiHttp = require('chai-http');
-
+var chai = require('chai');
+var expect = chai.expect;
+var chaiHttp = require('chai-http');
+var Scraper = require("../lib/Scraper.js");
 var request = require("request");
 
 describe("Scrapers", function () {
 
-  describe("Dribbble", function () {
-
-    it("is reachable", function () {
-
+  describe("Medium", function () {
+    it("has not changed dom structure", function (done) {
+      var bot = new Scraper.MediumScraper();
+      bot.scrape(function (articles) {
+        chai.expect(articles).not.to.have.length(0);
+        done();
+      });
     });
+  });
 
-    it("has not changed dom structure", function () {
 
+
+  describe("RGBToday", function () {
+    it("has not changed dom structure", function (done) {
+      var bot = new Scraper.RGBTodayScraper();
+      bot.scrape(function (articles) {
+        chai.expect(articles).not.to.have.length(0);
+        done();
+      });
     });
+  });
 
-
-
-
+  describe("Smashing Magazine", function () {
+    it("has not changed dom structure", function (done) {
+      var bot = new Scraper.SmashingMagazineScraper();
+      bot.scrape(function (articles) {
+        chai.expect(articles).not.to.have.length(0);
+        done();
+      });
+    });
   });
 
 
