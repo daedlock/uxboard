e60df997079e5fe45659d4f69c56e1e6127719ec
Hossam Saraya <hossam.saraya@gmail.com>
2015-05-19 15:08:59 +0300

    Refactored file names

--- bin/crons.js
+++ bin/crons.js
@@ -6,5 +6,5 @@ var rule = new schedule.RecurrenceRule();
 rule.minute = [53,0,03];
 var j = schedule.scheduleJob(rule, function(){
     //Runs scraper on all sources daily at 5,8 am
-    require("./scripts/runScraperOnce.js")();
+    require("./scripts/scrapeAllSources.js")();
 });
---
+++ bin/runOnce.js
@@ -0,0 +1,5 @@
+/**
+ * Created by deadlock on 5/19/15.
+ */
+
+require("./scripts/scrapeAllSources")();
