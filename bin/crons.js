c0f9a0dffe59bff0be9696f0acf9d876403aae90
Hossam Saraya <hossam.saraya@gmail.com>
2015-05-21 16:56:30 +0000

    time

--- bin/crons.js
+++ bin/crons.js
@@ -3,7 +3,7 @@
 var schedule = require('node-schedule');
 
 var rule = new schedule.RecurrenceRule();
-rule.minute = [53,0,03];
+rule.minute = [0,30];
 var j = schedule.scheduleJob(rule, function(){
     //Runs scraper on all sources daily at 5,8 am
     require("./scripts/scrapeAllSources.js")();
